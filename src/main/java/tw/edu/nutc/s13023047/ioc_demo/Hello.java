b8e08fa8f376b67bc0a0d2357aadfe757d7c87f9
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-05-30 13:50:21 +0800

    Add property

--- src/main/java/tw/edu/nutc/s13023047/ioc_demo/Hello.java
+++ src/main/java/tw/edu/nutc/s13023047/ioc_demo/Hello.java
@@ -2,13 +2,22 @@ package tw.edu.nutc.s13023047.ioc_demo;
 
 public class Hello {
 	
+	private String mGreet = "Hello";
 	private String mName;
 	
 	public Hello(String name) {
 		mName = name;
 	}
 	
+	public String getGreet() {
+		return mGreet;
+	}
+
+	public void setGreet(String greet) {
+		this.mGreet = greet;
+	}
+
 	public void hello() {
-		System.out.println("Hello " + mName);
+		System.out.println(mGreet + " " + mName);
 	}
 }
