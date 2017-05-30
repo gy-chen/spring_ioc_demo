806cf2c81f82402fcb91126f39fccffacdb111dd
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-05-30 11:20:03 +0800

    Add constructor-arg

--- src/main/java/tw/edu/nutc/s13023047/ioc_demo/Hello.java
+++ src/main/java/tw/edu/nutc/s13023047/ioc_demo/Hello.java
@@ -1,7 +1,14 @@
 package tw.edu.nutc.s13023047.ioc_demo;
 
 public class Hello {
+	
+	private String mName;
+	
+	public Hello(String name) {
+		mName = name;
+	}
+	
 	public void hello() {
-		System.out.println("Hello Spring Framwork!");
+		System.out.println("Hello " + mName);
 	}
 }
